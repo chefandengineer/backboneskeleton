3b254feb29aff7abbecab439fa2ae3db121ca305
chefandengineer <474667070@qq.com>
2014-05-11 20:43:20 -0400

    removed get food trucks by lat and lng

--- server.js
+++ server.js
@@ -126,6 +126,7 @@ var mongodbServer = restify.createServer({
 });
 
 mongodbServer.use(restify.bodyParser());
+mongodbServer.use(restify.queryParser());
 
 // Create a schema for our data
 var MessageSchema = new Schema({
@@ -139,12 +140,27 @@ var UserSchema = new Schema({
   user : String
 })
 
+//FoodTruck Schema
+var FoodTruckSchema = new Schema({
+  truckName : String,
+  locationId : Number,
+  locationDesc : String,
+  address : String,
+  foodItems : String,
+  latitude : Number,
+  longtitude : Number 
+})
+
 // Use the schema to register a model
 mongoose.model('Message', MessageSchema); 
 var MessageMongooseModel = mongoose.model('Message'); // just to emphasize this isn't a Backbone Model
 
 mongoose.model('User',UserSchema);
 var UserMongooseModel = mongoose.model('User');
+
+mongoose.model('FoodTruck',FoodTruckSchema);
+var FoodTruckMongooseModel = mongoose.model('FoodTruck');
+
 
 /*
 
@@ -232,7 +248,7 @@ var postUser = function(req, res, next) {
   }
   
   // Create a new message model, fill it up and save it to Mongodb
-  var user = new UserMongooseModel(); 
+var user = new UserMongooseModel(); 
   
   console.log("mongodbServer postMessage: " + req.params.user);
 
@@ -242,6 +258,123 @@ var postUser = function(req, res, next) {
   });
 }
 
+
+
+
+var FoodTruckSchema = new Schema({
+  locationId : Number,
+  locationDesc : String,
+  address : String,
+  foodItems : String,
+  latitude : Number,
+  longtitude : Number 
+})
+
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+
+var getFoodTrucks = function(req, res, next) {
+  var lat;
+  var lng;
+  console.log(req);
+
+  res.header( 'Access-Control-Allow-Origin', '*' );
+  res.header( 'Access-Control-Allow-Method', 'POST, GET, PUT, DELETE, OPTIONS' );
+  res.header( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-File-Name, Content-Type, Cache-Control' );
+  
+  if( 'OPTIONS' == req.method ) {
+    res.send( 203, 'OK' );
+  }
+  
+  console.log("mongodbServer getFoodTrucks");
+  //if lat and lng provided, get corresponding list
+  if(req && req.query && req.query.lat && req.query.lng) {
+    console.log(req.query);
+    lat = req.query.lat;
+    lng = req.query.lng;
+
+    console.log("Looking for food trucks near lat : " + lat + " , lng : " + lng);
+
+    FoodTruckMongooseModel.find().execFind(function (arr,data) {
+      var distances = {};
+      for (var i = data.length - 1; i >= 0; i--) {
+        var foodtruck = data[i];
+        var distance = getDistanceFromLatLonInKm(lat,lng,foodtruck.latitude,foodtruck.longtitude);
+        var id = data[i]._id;
+        distances[id] = distance;
+      }
+
+      data.sort(function(a,b){
+        var id1 = a._id;
+        var id2 = b._id;
+        var distance1 = distances[id1];
+        var distance2 = distances[id2];
+        console.log(distance1 + " compare to " + distance2);
+        if(distance1 < distance2) return -1;
+        if(distance1 > distance2) return 1;
+        return 0;
+     });
+    res.send(data.slice(0,20));
+  }); 
+  } else {
+    FoodTruckMongooseModel.find().limit(20).sort('truckName', -1).execFind(function (arr,data) {
+      res.send(data);
+    });
+  }
+}
+
+var getFoodTrucksByLocation = function(req,res,next){
+  res.header( 'Access-Control-Allow-Origin', '*' );
+  res.header( 'Access-Control-Allow-Method', 'POST, GET, PUT, DELETE, OPTIONS' );
+  res.header( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-File-Name, Content-Type, Cache-Control' );
+  
+  if( 'OPTIONS' == req.method ) {
+    res.send( 203, 'OK' );
+  }
+  
+  var lat = req.params.lat;
+  var lng = req.params.lng;
+
+
+  FoodTruckMongooseModel.find().execFind(function (arr,data) {
+     var sortedData = [];
+     for (var i = data.length - 1; i >= 0; i--) {
+       var foodtruck = data[i];
+       var distance = getDistanceFromLatLonInKm(lat,lng,foodtruck.latitude,foodtruck.longtitude);
+       var entry = {};
+       //Get the top 5 Sale items 
+       data[i].distance = distance;
+       foodtruck.distance = distance; 
+       // console.log(foodtruck);
+        entry.distance = distance;
+        entry.foodtruck = foodtruck;
+        sortedData.push(entry);
+     };
+     sortedData.sort(function(a,b){
+        var distance1 = a.distance;
+        var distance2 = b.distance;
+        if(distance1 < distance2) return -1;
+        if(distance1 > distance2) return 1;
+        return 0;
+     });
+    res.send(sortedData.slice(0,20));
+  }); 
+}
 
 mongodbServer.listen(mongodbPort, function() {
   
@@ -262,6 +395,9 @@ mongodbServer.post('/messages', postMessage);
 
 mongodbServer.get('/users', getUsers);
 mongodbServer.post('/users', postUser);
+
+mongodbServer.get('/foodtrucks/:lat/:lng',getFoodTrucksByLocation);
+mongodbServer.get('/foodtrucks',getFoodTrucks);
 
 
 
