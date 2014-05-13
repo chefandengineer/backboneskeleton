a889d37dcc3fffa93fb4fa0b1323cb76de8a8955
chefandengineer <474667070@qq.com>
2014-05-13 01:01:03 -0400

    commited all

--- config.js
+++ config.js
@@ -1,10 +1,4 @@
-// Don't commit this file to your public repos
 exports.creds = {
-  // Your mongo auth uri goes here
-  // e.g. mongodb://username:server@mongoserver:10059/somecollection
-  // nationalpark is the name of my mongo database
-  mongoose_auth_local: 'mongodb://heroku_app24825494:j03hj9n6hdtgvmpvdq6qi641a2@ds037478.mongolab.com:37478/heroku_app24825494',
-  mongoose_auth_jitsu: 'mongodb://heroku_app24825494:j03hj9n6hdtgvmpvdq6qi641a2@ds037478.mongolab.com:37478/heroku_app24825494'
-//  mongoose_auth_local: 'mongodb://localhost/nationalpark',
-//  mongoose_auth_jitsu: 'mongodb://localhost/nationalpark'
+  mongoose_auth_local: 'mongodb://localhost/foodtrucks',
+  mongoose_auth_heroku: 'mongodb://heroku_app24825494:j03hj9n6hdtgvmpvdq6qi641a2@ds037478.mongolab.com:37478/heroku_app24825494'
 }
--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 
-//http server
+/**
+  Restify server that serves both static files and service api
+**/
 var fs = require('fs');
-var httpServer = require('http');
+var httpServer = require('http'); // might not needed 
 var path = require('path');
 var connect = require('connect');
 //mongo server
 var mongoose = require('mongoose/');
 var restify = require('restify');  
-
 var config = require('./config');
 
-
-//var httpPort = process.env.PORT || 8080;
 var mongodbPort = process.env.PORT || 8080;
 
 
 var sendHTML = function( filePath, contentType, response ){
-
-  console.log('sendHTML: ' + filePath) ;
-
   path.exists(filePath, function( exists ) {
         if (exists) {
             fs.readFile(filePath, function(error, content) {
@@ -40,23 +36,18 @@ var sendHTML = function( filePath, contentType, response ){
 }
 
 var getFilePath = function(url) {
-
   var filePath = './app' + url;
   if (url == '/' ) { 
       filePath = './app/index.html';
   } else {
      filePath =  filePath.replace("/docs","");
   }
-
-
   return filePath;
 }
 
 var getContentType = function(filePath) {
-   
    var extname = path.extname(filePath);
    var contentType = 'text/html';
-    
     switch (extname) {
         case '.js':
             contentType = 'text/javascript';
@@ -65,26 +56,18 @@ var getContentType = function(filePath) {
             contentType = 'text/css';
             break;
     }
-
     return contentType;
 }
 
 var onHtmlRequestHandler = function(request, response) {
-
-  console.log('onHtmlRequestHandler... request.url: ' + request.url) ;
-
+  //serving static data
   var filePath = getFilePath(request.url);
   var contentType = getContentType(filePath);
-
-  console.log('onHtmlRequestHandler... getting: ' + filePath) ;
   sendHTML(filePath, contentType, response); 
 }
 
-//httpServer.createServer(onHtmlRequestHandler).listen(httpPort); 
 
-////////////////////////////////////////////////////// MONGODB - saves data in the database and posts data to the browser
-
-var mongoURI = ( process.env.PORT ) ? config.creds.mongoose_auth_local : config.creds.mongoose_auth_local;
+var mongoURI = ( process.env.PORT ) ? config.creds.mongoose_auth_heroku : config.creds.mongoose_auth_local;
 
 db = mongoose.connect(mongoURI),
 Schema = mongoose.Schema;  
@@ -108,17 +91,6 @@ var mongodbServer = restify.createServer({
 mongodbServer.use(restify.bodyParser());
 mongodbServer.use(restify.queryParser());
 
-// Create a schema for our data
-var MessageSchema = new Schema({
-  message: String,
-  date: Date
-});
-
-
-//User 
-var UserSchema = new Schema({
-  user : String
-})
 
 //FoodTruck Schema
 var FoodTruckSchema = new Schema({
@@ -131,102 +103,9 @@ var FoodTruckSchema = new Schema({
   latitude : Number
 })
 
-// Use the schema to register a model
-mongoose.model('Message', MessageSchema); 
-var MessageMongooseModel = mongoose.model('Message'); // just to emphasize this isn't a Backbone Model
-
-mongoose.model('User',UserSchema);
-var UserMongooseModel = mongoose.model('User');
-
 mongoose.model('FoodTruck',FoodTruckSchema);
 var FoodTruckMongooseModel = mongoose.model('FoodTruck');
 
-
-
-// This function is responsible for returning all entries for the Message model
-var getMessages = function(req, res, next) {
-  // Resitify currently has a bug which doesn't allow you to set default headers
-  // This headers comply with CORS and allow us to mongodbServer our response to any origin
-  res.header( 'Access-Control-Allow-Origin', '*' );
-  res.header( 'Access-Control-Allow-Method', 'POST, GET, PUT, DELETE, OPTIONS' );
-  res.header( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-File-Name, Content-Type, Cache-Control' );
-  
-  if( 'OPTIONS' == req.method ) {
-    res.send( 203, 'OK' );
-  }
-  
-  console.log("mongodbServer getMessages");
-
-  MessageMongooseModel.find().limit(20).sort('date', -1).execFind(function (arr,data) {
-    res.send(data);
-  });
-}
-
-var postMessage = function(req, res, next) {
-  res.header( 'Access-Control-Allow-Origin', '*' );
-  res.header( 'Access-Control-Allow-Method', 'POST, GET, PUT, DELETE, OPTIONS' );
-  res.header( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-File-Name, Content-Type, Cache-Control' );
-  
-  if( 'OPTIONS' == req.method ) {
-    res.send( 203, 'OK' );
-  }
-  
-  // Create a new message model, fill it up and save it to Mongodb
-  var message = new MessageMongooseModel(); 
-  
-  console.log("mongodbServer postMessage: " + req.params.message);
-
-  message.message = req.params.message;
-  message.date = new Date() 
-  message.save(function () {
-    res.send(req.body);
-  });
-}
-
-var getUsers = function(req,res,next){
-  res.header( 'Access-Control-Allow-Origin', '*' );
-  res.header( 'Access-Control-Allow-Method', 'POST, GET, PUT, DELETE, OPTIONS' );
-  res.header( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-File-Name, Content-Type, Cache-Control' );
-  
-  if( 'OPTIONS' == req.method ) {
-    res.send( 203, 'OK' );
-  }
-  
-  console.log("mongodbServer getUsers");
-
-  UserMongooseModel.find().limit(20).sort('name', -1).execFind(function (arr,data) {
-    res.send(data);
-  });
-}
-
-var postUser = function(req, res, next) {
-  res.header( 'Access-Control-Allow-Origin', '*' );
-  res.header( 'Access-Control-Allow-Method', 'POST, GET, PUT, DELETE, OPTIONS' );
-  res.header( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-File-Name, Content-Type, Cache-Control' );
-  
-  if( 'OPTIONS' == req.method ) {
-    res.send( 203, 'OK' );
-  }
-  
-  // Create a new message model, fill it up and save it to Mongodb
-var user = new UserMongooseModel(); 
-  
-  console.log("mongodbServer postMessage: " + req.params.user);
-  user.user = req.params.user;
-  user.save(function () {
-    res.send(req.body);
-  });
-}
-
-
-var FoodTruckSchema = new Schema({
-   locationId : Number,
-   locationDesc : String,
-   address : String,
-   foodItems : String,
-   latitude : Number,
-   longtitude : Number 
- })
 
 function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
   var R = 6371; // Radius of the earth in km
@@ -297,60 +176,20 @@ var getFoodTrucks = function(req, res, next) {
   }
 }
 
-var getFoodTrucksByLocation = function(req,res,next){
-  res.header( 'Access-Control-Allow-Origin', '*' );
-  res.header( 'Access-Control-Allow-Method', 'POST, GET, PUT, DELETE, OPTIONS' );
-  res.header( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-File-Name, Content-Type, Cache-Control' );
-  
-  if( 'OPTIONS' == req.method ) {
-    res.send( 203, 'OK' );
-  }
-  
-  var lat = req.params.lat;
-  var lng = req.params.lng;
-
-
-  FoodTruckMongooseModel.find().execFind(function (arr,data) {
-     var sortedData = [];
-     for (var i = data.length - 1; i >= 0; i--) {
-       var foodtruck = data[i];
-       var distance = getDistanceFromLatLonInKm(lat,lng,foodtruck.latitude,foodtruck.longtitude);
-       var entry = {};
-       //Get the top 5 Sale items 
-       data[i].distance = distance;
-       foodtruck.distance = distance; 
-       // console.log(foodtruck);
-        entry.distance = distance;
-        entry.foodtruck = foodtruck;
-        sortedData.push(entry);
-     };
-     sortedData.sort(function(a,b){
-        var distance1 = a.distance;
-        var distance2 = b.distance;
-        if(distance1 < distance2) return -1;
-        if(distance1 > distance2) return 1;
-        return 0;
-     });
-    res.send(sortedData.slice(0,20));
-  }); 
-}
 
 mongodbServer.listen(mongodbPort, function() {
   
   var consoleMessage = '\n San Francisco Food Truck Application! Server Started';
-  consoleMessage += '\n +++++++++++++++++++++++++++++++++++++++++++++++++++++' 
-  consoleMessage += '\n\n %s says your mongodbServer is listening at %s';
-  consoleMessage += '\n great! now open your browser to http://localhost:8080';
-  consoleMessage += '\n it will connect to your httpServer to get your static files';
-  consoleMessage += '\n and talk to your mongodbServer to get and post your messages. \n\n';
   consoleMessage += '+++++++++++++++++++++++++++++++++++++++++++++++++++++ \n\n'  
  
   console.log(consoleMessage, mongodbServer.name, mongodbServer.url);
 
 });
 
-
+//Rest APIs
+//food trucks, get food trucks 
 mongodbServer.get('/foodtrucks',getFoodTrucks);
+//static files 
 mongodbServer.get('/',onHtmlRequestHandler);
 mongodbServer.get('/docs/.*',onHtmlRequestHandler);
 
