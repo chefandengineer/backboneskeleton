cb49e7997bac95b2d3a5442a154752c97808d043
chefandengineer <474667070@qq.com>
2014-05-12 22:59:20 -0400

    deleted

--- configSample.js
+++
@@ -1,5 +0,0 @@
-// Don't commit this file to your public repos
-exports.creds = {
-  mongoose_auth_local: 'mongodb://heroku_app24825494:j03hj9n6hdtgvmpvdq6qi641a2@ds037478.mongolab.com:37478/heroku_app24825494',
-  mongoose_auth_jitsu: 'copy and paste your unique connection string uri from the nodejitsu admin'
-}
